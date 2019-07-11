0124a74738e480d438ec58fb8154429855a30b33
qwer5656 <qwer5656@gamil.com>
2019-07-11 10:45:25 +0800

    add isArrayLike function

--- test.js
+++ test.js
@@ -1,1 +1,35 @@
-alert("hello github1");
+function isArrayLike(obj)
+{
+    let len=obj.length||-1;
+   if(obj&&typeof obj=="object"&&isFinite(len)&&len>=0&&len<Math.pow(2,32)&&len===parseInt(len))
+   {
+       return true;
+   }
+   return false;
+}
+let obj={
+    0:1,
+    1:2,
+    2:2,
+    3:3,
+    length:4,
+}
+function sum(obj)
+{
+    var sum=0;
+    if(isArrayLike(obj))
+    {
+        for(var i=0;i<obj.length;i++)
+        {
+            var ele=obj[i];
+            if(ele==null)continue;
+            if(isFinite(ele))sum+=ele;
+            else
+            console.log(`ele ${i} is not number`);
+        }
+    }
+    else
+    console.log(`arguments is not arraylike`);
+    return sum;
+}
+console.log(sum(obj));
